332187231bcd5cd6cf32c75844988600d864c04a
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-07-15 12:59:46 +0200

    Update Prompt-Computer-Restart.ps1

--- Prompt-Computer-Restart.ps1
+++ Prompt-Computer-Restart.ps1
@@ -4,58 +4,66 @@ $TimeTriger = $LastBootTime -lt (Get-Date).AddDays(-7)
 
 if ($TimeTriger) {
     $WindowTitle = "Obaveštenje za restart računara"
-    $Message = "Poštovani, Vaš računar nije restartovan više od nedelju dana.`r`n
-    Molim Vas da restartujete računar kako ne bi došlo do problema u radu Vašeg računara.`r`n
-    Ukoliko sami ne restartujete računar on će se sam restartovati u roku od sedam sati.`r`n`r`n
-    IT sektor, Univerexport doo"
+    $Message = "Poštovani, Vaš računar nije restartovan više od nedelju dana. Molim Vas da restartujete računar kako ne bi došlo do problema u radu Vašeg računara. Ukoliko sami ne restartujete računar on će se sam restartovati u roku od sedam sati.`r`n`r`nIT sektor, Univerexport doo"
     $RestartButtonName = "Restartuj odmah"
     $PostponeButtonName = "Odloži"
 
+    $Margin = 30
+    $WindowHeight = 400
+    $WindowWidth = 610
+    $ButtonWith = 200
+    $ButtonHeight = 50
+    
     Add-Type -AssemblyName System.Windows.Forms
     [System.Windows.Forms.Application]::EnableVisualStyles()
 
-    $PromptComputerRestartForm = New-Object system.Windows.Forms.Form
-    $PromptComputerRestartForm.ClientSize = New-Object System.Drawing.Point(800, 350)
-    $PromptComputerRestartForm.text = $WindowTitle
-    $PromptComputerRestartForm.TopMost = $true
+    $Window = New-Object system.Windows.Forms.Form
+    $Window.Text = $WindowTitle
+    $Window.TopMost = $true
+    $Window.Height = $WindowHeight
+    $Window.Width = $WindowWidth
+    $Window.ControlBox = $false
+    $Window.ShowIcon = $false
+    $Window.FormBorderStyle = 'Fixed3D'
+    $Window.MaximizeBox = $false
+    $Window.Padding = 30
 
     $MessageField = New-Object system.Windows.Forms.Label
-    $MessageField.text = $Message
-    $MessageField.AutoSize = $true
-    $MessageField.width = 500
-    $MessageField.height = 300
-    $MessageField.location = New-Object System.Drawing.Point(50, 50)
-    $MessageField.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 10)
+    $MessageField.Text= $Message
+    $MessageField.Height = 150
+    $MessageField.Width = 550
+    $MessageField.Location = New-Object System.Drawing.Point(30, 30)
+    $MessageField.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 13)
 
     $RestartButton = New-Object system.Windows.Forms.Button
-    $RestartButton.text = $RestartButtonName
-    $RestartButton.width = 160
-    $RestartButton.height = 50
-    $RestartButton.location = New-Object System.Drawing.Point(210, 170)
-    $RestartButton.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 10)
-    $RestartButton.Add_Click( {
+    $RestartButton.Text = $RestartButtonName
+    $RestartButton.Width = $ButtonWith
+    $RestartButton.Height = $ButtonHeight
+    $RestartButton.Location = New-Object System.Drawing.Point($Margin, 210)
+    $RestartButton.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 13)
+    $RestartButton.Add_Click({
         Restart-Computer -Force
     })
 
     $PostponeButton = New-Object system.Windows.Forms.Button
-    $PostponeButton.text = $PostponeButtonName
-    $PostponeButton.width = 160
-    $PostponeButton.height = 50
-    $PostponeButton.location = New-Object System.Drawing.Point(450, 170)
-    $PostponeButton.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 10)
-    $PostponeButton.Add_Click( {
-        $PromptComputerRestartForm.Close()
+    $PostponeButton.Text = $PostponeButtonName
+    $PostponeButton.Width = $ButtonWith
+    $PostponeButton.Height = $ButtonHeight
+    $PostponeButton.Location = New-Object System.Drawing.Point(($WindowWidth - $ButtonWith - $Margin), 210)
+    $PostponeButton.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 13)
+    $PostponeButton.Add_Click({
+        $Window.Close()
     })
 
-    $PromptComputerRestartForm.controls.AddRange(@($MessageField, $RestartButton, $PostponeButton))
+    $Window.controls.AddRange(@($MessageField, $RestartButton, $PostponeButton))
 
     Start-Job -ScriptBlock {
-        Start-Sleep -Seconds 2520
+        Start-Sleep -Seconds 25200
         Restart-Computer -Force
     }
 
     while ($TimeTriger) {
-        [void]$PromptComputerRestartForm.ShowDialog()
+        [void]$Window.ShowDialog()
         Start-Sleep -Seconds 3600
     }
 }
